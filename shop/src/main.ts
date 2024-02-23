a0e808f596dfc03d30aa539f115c589890353711
DANH PHI LONG <longgggwww@gmail.com>
2024-02-23 16:08:24 +0700

    change shop mongodb to postgres

--- shop/src/main.ts
+++ shop/src/main.ts
@@ -1,43 +1,8 @@
-import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { HttpAdapterHost, NestFactory } from '@nestjs/core';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
-import { PrismaClientExceptionFilter } from 'nestjs-prisma';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { TrimPipe } from './pipes/trim.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const cfg = app.get(ConfigService);
-
-  app.enableCors();
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transformOptions: {
-        enableImplicitConversion: true,
-      },
-    }),
-    new TrimPipe(),
-  );
-
-  const { httpAdapter } = app.get(HttpAdapterHost);
-  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
-
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.RMQ,
-    options: {
-      urls: [`${cfg.get('RB_URL')}`],
-      queue: `${cfg.get('QUEUE')}`,
-      queueOptions: {
-        durable: true,
-      },
-    },
-  });
-
-  await app.startAllMicroservices();
-  await app.listen(cfg.get('PORT', 3000));
+  await app.listen(3000);
 }
 bootstrap();
